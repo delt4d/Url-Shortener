aa8d7816b220b14737870c7b19892ea54adf8d2f
Leandro G. da Cruz <LeandroCruz717@outlook.com>
2025-05-17 22:37:03 -0300

    chore: clean frontend project

--- Frontend/Frontend/Program.cs
+++ Frontend/Frontend/Program.cs
@@ -21,15 +21,11 @@ if (app.Environment.IsDevelopment())
 else
 {
     app.UseExceptionHandler("/Error", createScopeForErrors: true);
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+    app.UseHsts(); // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
 }
 
 app.UseHttpsRedirection();
-
-
 app.UseAntiforgery();
-
 app.MapStaticAssets();
 app.MapRazorComponents<App>()
     .AddInteractiveWebAssemblyRenderMode()
